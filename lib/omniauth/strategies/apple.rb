564a75d5aa20b6c3eef7539c380812f104d7131d
Pavel Shapovalov <shh.pa@outlook.com>
2021-07-27 15:13:06 +0300

    tst

--- lib/omniauth/strategies/apple.rb
+++ lib/omniauth/strategies/apple.rb
@@ -48,7 +48,7 @@ module OmniAuth
 
       # from https://meta.discourse.org/t/sign-in-with-apple/122790/40
       def callback_phase
-        if request.request_method.downcase.to_sym == :post && session[:state] == nil
+        if request.request_method.downcase.to_sym == :post && request.cookies[ request.session_options[:session_key] ] == nil
           url = "#{callback_url}"
           if (code = request.params['code']) && (state = request.params['state'])
             url += "?code=#{CGI::escape(code)}"
